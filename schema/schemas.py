0d459c90c2049b827cbb147537a190d31f8a03d3
ddg <1548199084@qq.com>
2026-04-02 01:13:20 +0800

    2026/04/01提交2

--- schema/schemas.py
+++ schema/schemas.py
@@ -1,19 +1,24 @@
 from pydantic import BaseModel, Field
-from typing import Optional
+from typing import Any
 
 
-class function_call(BaseModel):
+class FunctionCall(BaseModel):
     function_name: str
-    args: Optional[BaseModel]
+    args: B
 
 
-class tool_call(BaseModel):
+class ToolCall(BaseModel):
     tool_call: str
-    type: str   # 目前仅有调用函数 type = function
-    function: function_call
+    type: str = 'function'   # 目前仅有调用函数 type = function
+    function: FunctionCall
 
 class Message(BaseModel):
+    """ 框架内消息结构 """
+
     role: str # system, user, assistant, tool
-    content: str
-    thinking: Optional[str] = None
-    tool_calls: Optional[list[tool_call]] = None
+    thinking: str | None
+    content: str | list[dict[str, Any]]
+    tool_calls: list[ToolCall] = None
+    # 当role为tool时，下面两个属性的值不应为空，tool_calls需要为空
+    tool_call_id: str = None
+    tool_call_name: str = None
